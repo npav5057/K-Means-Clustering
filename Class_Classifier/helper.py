e5c1efe6656323e510db48c0865f98ef92def850
Pradeep Nag <36629263+npav5057@users.noreply.github.com>
2019-11-03 10:54:39 +0530

    K Means Classifier code Uploaded

---
+++ Class_Classifier/helper.py
@@ -0,0 +1,61 @@
+from random import choice
+from math import hypot,sqrt,pow
+
+
+def mean(Class):
+    mn =[0,0]
+    for data in Class:
+        for j in range(2):
+            mn[j] = mn[j]+data[j]
+    for j in range(2):
+        mn[j] = mn[j]/len(Class)
+    return mn
+
+def get_random_colour(number_of_colors):
+    color = ["#"+''.join([choice('0123456789ABCDEF') for j in range(6)])for i in range(number_of_colors)]
+    return color
+
+
+
+
+def get_Cluster(point,mean,k):
+    min_dist=9999999.9
+    index= -1
+    for i in range(k):
+        dist=hypot(point[0]-mean[i][0],point[1]-mean[i][1])
+        if min_dist>dist:
+            index=i
+            min_dist = dist
+    #print(index)
+    return index,min_dist
+
+
+def set_random_mean(data,K):
+    means = []
+    for i in range(K):
+        means.append(choice(data))
+    return means
+
+
+
+def get_datapoints(file):
+    f=open(file,"r")
+    X=[]
+    for line in f:
+        a,b=line.split()
+        X.append([float(a),float(b)])
+    f.close()
+    return X
+
+def get_data(file):
+    f=open(file,"r")
+    X=[]
+    flag = True
+    for line in f:
+        if flag is True:
+            flag =False
+        else:
+            a,b=line.split()
+            X.append([float(a),float(b)])
+    f.close()
+    return X
---
+++ Class_Classifier/main.py
@@ -0,0 +1,43 @@
+import helper as fun
+import sys
+from matplotlib import pyplot as plt
+
+error =0.05
+
+if __name__ == '__main__':
+
+    k=int(sys.argv[2])
+    if(sys.argv[1]=='LS'):
+        data = fun.get_datapoints("Data1/Class1.txt")
+        data = data + fun.get_datapoints("Data1/Class3.txt")
+        data = data + fun.get_datapoints("Data1/Class2.txt")
+    elif(sys.argv[1]=='NLS'):
+        data = fun.get_data("Data2/mixed_data.txt")
+
+    # print(len(data))
+    # K points are chosen randomly from data as mean
+    mean = fun.set_random_mean(data,k)
+
+    itr = 0
+    final=999999.9
+    init=0.0
+    while(abs(final-init)>error):
+        cluster = [[] for i in range(k)]
+        itr=itr+1
+        init=final
+        final=0
+        for point in data:
+            assigned_cluster,min_distortion = fun.get_Cluster(point,mean,k)
+            cluster[assigned_cluster].append(point)
+            final=final+min_distortion
+        for i in range(k):
+            mean[i]=fun.mean(cluster[i])
+        print("Iteration:",itr,"     Distortion measure:",abs(final-init))
+
+
+    colour=fun.get_random_colour(k)
+    for t in range(k):
+        for data in cluster[t]:
+            plt.plot(data[0],data[1],color=colour[t],marker='o', markersize=1)
+
+    plt.savefig(sys.argv[1]+"_cluster_"+str(k)+".png")
